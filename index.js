bf7ce6702abd016f64d7b00590301db8ece19b20
DESKTOP-SKERJDK\saloo <oleg.d.salo@gmail.com>
2021-07-25 19:27:52 +0300

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import cors from "cors";
 
 // const express = require('express');
+// var cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
+
+
+app.get('/products/:id', function (req, res, next) {
+    res.json({ msg: 'hello world' })
+})
+
+app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+})
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -11,3 +23,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
